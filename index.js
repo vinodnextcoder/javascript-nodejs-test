96fd47baab64e190a9940f207fe89a74a28b7f89
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-04 00:05:14 +0530

    add end point to start
    added routes api,readme cange

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("/url", (req, res, next) => {
+app.get("/", (req, res, next) => {
   let user = { userList: ["vinod pawar", "Nitin Pawar", "Rauhl Pawar"] };
   res.json(user);
 
