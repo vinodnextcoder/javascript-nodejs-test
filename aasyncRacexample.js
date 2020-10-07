e0ef1ec9a1d6aa236bca22f9d4f104a50512445c
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-07 10:23:22 +0530

    find in array of object
    get elment in array

--- aasyncRacexample.js
+++ aasyncRacexample.js
@@ -1,22 +1,41 @@
 var async = require("async");
 var asyncCtrl = {};
-function asyncRaceExample(req, res) {
-    async.race([
-        function (callback) {
-            setTimeout(function () {
-                callback(null, 'one');
-            }, 300);
-        },
-        function (callback) {
-            setTimeout(function () {
-                callback(null, 'two');
-            }, 100);
+var contacts = [
+    {
+        "firstName": "pradip",
+        "lastName": "Nashik",
+        "number": "9943236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Nitint",
+        "lastName": "Pod",
+        "number": "999999",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    }
+];
+function lookUpProfile(firstName, prop){
+    for(var i=0; i<contacts.length ;i++){
+      if(contacts[i].firstName === firstName){
+        if(contacts[i].hasOwnProperty(prop)){
+          return contacts[i][prop];
+        }else{
+          return "No such property";
         }
-    ],
-    function (err, result) {
-        console.log('The result is ', result);
-        res.json(result);
-    });
-};
+      }
+    }return "No such contact";
+}
+    function asyncRaceExample(req, res) {
+        async.race([
+            function (callback) {
+                let msg = lookUpProfile("pradip", "likes");
+                callback(null, msg)
+            }
+        ],
+            function (err, result) {
+                console.log('The result is ', result);
+                res.json(result);
+            });
+    };
 asyncCtrl.asyncRaceExample = asyncRaceExample
 module.exports = asyncCtrl;
