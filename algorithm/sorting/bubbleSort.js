dfd0f54f6820b7d1fa98af928bd07d058937dd44
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-13 23:35:45 +0530

    second example for bubble sort

--- algorithm/sorting/bubbleSort.js
+++ algorithm/sorting/bubbleSort.js
@@ -13,6 +13,24 @@ let bubbleSort = (inputArr) => {
   }
   return inputArr;
 };
+function bubble_Sort(a) {
+  var swapp;
+  var n = a.length - 1;
+  var x = a;
+  do {
+    swapp = false;
+    for (var i = 0; i < n; i++) {
+      if (x[i] > x[i + 1]) {
+        var temp = x[i];
+        x[i] = x[i + 1];
+        x[i + 1] = temp;
+        swapp = true;
+      }
+    }
+    n--;
+  } while (swapp);
+  return x;
+}
 // The idea behind bubble sort is that every pair of adjacent values is compared, and then the two values swap positions if the first value is greater than the second. This way during each pass through the array, the largest value “bubbles up” to the top, and after each pass the elements furthest to the right are in the correct order. Here’s an example: given the array [5,3,1,4,6] we can use bubble sort to sort the array in ascending order. It’ll start by comparing the first pair of values, 5 & 3. 3 is smaller than 5, so it’ll swap the two values and then move on to compare the next pair of values, 5 & 1. It’ll continue doing this over and over until the array is sorted:
 // Iteration 1: [5,3,1,4,6] → [3,5,1,4,6] → [3,1,5,4,6] → [3,1,4,5,6] → [3,1,4,5,6]
 // Iteration 2: [3,1,4,5,6] → [1,3,4,5,6] → [1,3,4,5,6] → [1,3,4,5,6] → [1,3,4,5,6]
@@ -20,3 +38,6 @@ let bubbleSort = (inputArr) => {
 var rainbow =[5,3,1,4,6];
 let output=bubbleSort(rainbow);
 console.log(">>>>>>>>>>>>>>>",output);
+var rainbow1 =[5,3,1,4,6];
+let output1=bubble_Sort(rainbow1);
+console.log(">>>>>>>>>>>>>>>",output1);
