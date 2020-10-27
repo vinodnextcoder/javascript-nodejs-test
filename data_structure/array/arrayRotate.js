be9f6fe948f877b919aec9963a05031ea736c0ab
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-28 00:34:29 +0530

    array rotation
    array rotate by split

--- data_structure/array/arrayRotate.js
+++ data_structure/array/arrayRotate.js
@@ -21,9 +21,36 @@ console.log(temp);
 
 console.log("####################SECOND######################");
 
+const rotationArray = (arr, d, n) => {
+    d = d % n
+    let g_c_d = findGCD(d, n)
+    for (i = 0; i < g_c_d; i++) {
+        temp = arr[i]
+        j = i
+        while (1) {
+            k = j + d
+            console.log(k,n);
+            if (k >= n)
+                k = k - n
+            if (k == i)
+                break
+            arr[j] = arr[k]
+            j = k
+        }
+        arr[j] = temp
+    }
+    return arr;
+};
+
 const findGCD = (a, b) => {
     if (b == 0)
         return a;
     else
-        return gcd(b, a % b)
+        return findGCD(b, a % b)
 };
+
+let arrr = [1, 2, 3, 4, 5, 6, 7] 
+let n = arrr.length
+let d = 2
+let f=rotationArray(arr, d, n);
+console.log(f)
