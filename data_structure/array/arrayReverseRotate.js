0b79ec53625a1b62f84cf1b7d448e2a4b0f9153a
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-29 10:21:00 +0530

    array rotation learning
    Program for reversal algorithm of array rotation
     Function to reverse arr[] from index start to end

--- data_structure/array/arrayReverseRotate.js
+++ data_structure/array/arrayReverseRotate.js
@@ -6,6 +6,7 @@
 // Reverse B, we get ArBr = [2, 1, 7, 6, 5, 4, 3]
 // Reverse all, we get (ArBr)r = [3, 4, 5, 6, 7, 1, 2]
 let reverseArray = (arr, start, end) => {
+    let temp
     while (start < end) {
         temp = arr[start]
         arr[start] = arr[end]
@@ -15,14 +16,25 @@ let reverseArray = (arr, start, end) => {
     }
 }
 
-let reverseArray = (arr, d) => {
-    if (d == 0):
+let leftRotate = (arr, d) => {
+    if (d == 0)
         return
-    n = len(arr)
+    n = arr.length
     // # in case the rotating factor is
     // # greater than array length
     d = d % n
     reverseArray(arr, 0, d - 1)
     reverseArray(arr, d, n - 1)
     reverseArray(arr, 0, n - 1)
+
+    return arr;
 }
+
+// # Driver function to test above functions
+arr = [1, 2, 3, 4, 5, 6, 7]
+n = arr.length
+d = 2
+ 
+let temp=leftRotate(arr, d)
+
+console.log(temp)
