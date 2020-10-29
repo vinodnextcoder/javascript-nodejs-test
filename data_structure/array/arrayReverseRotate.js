b8a69797b076296a9a5e0dc59f1d5ae1f729ef13
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-29 10:16:09 +0530

    rotate array

--- data_structure/array/arrayReverseRotate.js
+++ data_structure/array/arrayReverseRotate.js
@@ -14,3 +14,15 @@ let reverseArray = (arr, start, end) => {
         end = end - 1
     }
 }
+
+let reverseArray = (arr, d) => {
+    if (d == 0):
+        return
+    n = len(arr)
+    // # in case the rotating factor is
+    // # greater than array length
+    d = d % n
+    reverseArray(arr, 0, d - 1)
+    reverseArray(arr, d, n - 1)
+    reverseArray(arr, 0, n - 1)
+}
