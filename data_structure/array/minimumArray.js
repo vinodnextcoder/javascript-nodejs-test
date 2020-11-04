191b76fba39e2bc6b5e16d2503bbe4932c61de7c
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-04 20:42:36 +0530

    find a minimum

--- data_structure/array/minimumArray.js
+++ data_structure/array/minimumArray.js
@@ -1,3 +1,10 @@
+A simple solution is to traverse the complete array and find a minimum. This solution requires O(n) time. 
+// We can do it in O(Logn) using Binary Search. If we take a closer look at the above examples, we can easily figure out the following pattern:
+
+// The minimum element is the only element whose previous is greater than it. If there is no previous element, then there is no rotation (the first element is minimum). We check this condition for the middle element by comparing it with (mid-1)’th and (mid+1)’th elements.
+// If the minimum element is not at the middle (neither mid nor mid + 1), then the minimum element lies in either the left half or right half. 
+// If the middle element is smaller than the last element, then the minimum element lies in the left half
+// Else minimum element lies in the right half.
 const findMin = (arr, low, high) => {
     // # This condition is needed to handle the case when array is not 
     // # rotated at all 
