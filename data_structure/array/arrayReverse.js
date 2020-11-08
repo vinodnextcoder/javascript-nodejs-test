b55fcba4f5608f7f5fea60e5a0a001e9fa221313
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-08 20:56:13 +0530

    js reverse nested array

--- data_structure/array/arrayReverse.js
+++ data_structure/array/arrayReverse.js
@@ -11,3 +11,10 @@ let arrr = [2, 2, 3, 12, 5, 6, 7]
 let n = arrr.length
 let f=reverseList(arrr, 0, n-1);
 console.log(f)
+let array =[[2, 2, 3, 12, 5, 6, 7] ]
+var reversed = array.map(function reverse(item) {
+    return Array.isArray(item) && Array.isArray(item[0]) 
+               ? item.map(reverse) 
+               : item.reverse();
+});
+console.log(reversed)
