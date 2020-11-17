ff40d118d3ef1185611a056b7cc175316d0916b4
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-17 11:10:05 +0530

    array load

---
+++ data_structure/array/arrayload.js
@@ -0,0 +1,12 @@
+function load() {
+    var list = [];
+    var keyArray = ["one","two","three","four","five"];
+  for (var i = 0; i < keyArray.length; i++) {
+        var item = localStorage.getItem(keyArray[i]);
+       if (item.charAt(0) == "+") {
+            item = parseFloat(item.substring(1));
+      }
+        list.push(item);
+  }
+    return list;
+}
