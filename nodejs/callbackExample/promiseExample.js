349e45dd79ab8197e1fa6dbebcf8adc86dacb1f2
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-12-01 00:12:43 +0530

    Promise in javascript
    Promises are another way to write asynchronous code that help you avoid deeply nested callback functions, also known as "callback hell." A promise can be in one of three states: pending, resolved, or rejected. Once a promise is resolved, you can handle the response using the promise.then() method. If a promise is rejected, you can handle the error using the promise.catch() method.

---
+++ nodejs/callbackExample/promiseExample.js
@@ -0,0 +1,20 @@
+var Promise = require('promise');
+
+function AsyncCall() {
+  console.log("Asyn call Exmaple Started")
+
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000) 
+  }) 
+}
+
+function Promisecall() {
+  console.log('Promise Executtion started')
+  let newArray = [1, 2, 3, 4, 5].map(function(number){
+    return number * 2
+})
+console.log("#######>>>######",newArray)
+  console.log('Promise Executed')
+}
+
+AsyncCall().then(Promisecall)
