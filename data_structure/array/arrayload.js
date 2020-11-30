e0558ceb5b51e40a35d060f71f8a6530b8a15314
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-30 23:31:36 +0530

    cutome filter for number >6
    In for loop we are looping through this. If the fun returns true ( element is greater than 6) that element will be pushed in filtered variable. After for loop we return filtered variable

--- data_structure/array/arrayload.js
+++ data_structure/array/arrayload.js
@@ -23,3 +23,18 @@ function arrayFilter(arr, func) {
   let filteredArray = arr.filter(func);
   return filteredArray[0] ? filteredArray[0] : undefined;
 }
+
+
+Array.prototype.mfilter =  function (fun) {
+  var filtered = [];
+  for(let i = 0; i < this.length; i++) {
+    if (fun(this[i], i, this)) filtered.push(this[i]);
+  }
+  return filtered;
+};
+
+var returnedArr = [1,2,3,4,5,6].mfilter(function(element, index, arr) {
+  return element > 5;
+});
+
+console.log(returnedArr);
