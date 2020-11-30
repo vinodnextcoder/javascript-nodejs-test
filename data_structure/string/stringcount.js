b9ea2d172bbd237bfa4dd9c9134fd555110a10ea
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-11-30 23:43:08 +0530

    Finding the Most Recurring Character
    keep track of every character in the string as well as the number of times it exists. This we would describe as character mapping

--- data_structure/string/stringcount.js
+++ data_structure/string/stringcount.js
@@ -1,10 +1,35 @@
 // We need to keep track of every character in the string as well as the number of times it exists.
-let charMap = {};
+// let charMap = {};
 
-for (let char of text) {
-    if (charMap.hasOwnProperty(char)) {
-        charMap[char]++
-    } else {
-        charMap[char] = 1;
-    }
+// for (let char of text) {
+//     if (charMap.hasOwnProperty(char)) {
+//         charMap[char]++
+//     } else {
+//         charMap[char] = 1;
+//     }
+// }
+
+function maxRecurringChar(text) {
+  let charMap = {}
+  let maxCharValue = 0
+  let maxChar = ''
+
+  for (let char of text) {
+      if (charMap.hasOwnProperty(char)) {
+          charMap[char]++
+      } else {
+          charMap[char] = 1
+      }
+  }
+
+  for (let char in charMap) {
+      if (charMap[char] > maxCharValue) {
+          maxCharValue = charMap[char]
+          maxChar = char
+      }
+  }
+
+  return maxChar
 }
+let temp=maxRecurringChar('vindiiiii') 
+console.log("Most recurring character",temp)
