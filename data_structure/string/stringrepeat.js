d6fcf31182f6e4fa91654e5e59a0cf0fa390f6ab
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2021-01-25 00:31:31 +0530

    revesrse string with loop

--- data_structure/string/stringrepeat.js
+++ data_structure/string/stringrepeat.js
@@ -13,4 +13,13 @@ function isIsogram(str){
   
  console.log(isIsogram("Dermatoglyphics")) // true
  console.log(isIsogram("aba"))// false
-  
+
+ function reverseString(str) {
+  var newString = "";
+  for (var i = str.length - 1; i >= 0; i--) { 
+      newString += str[i]; // or newString = newString + str[i];
+  }
+  return newString; // "olleh"
+}
+
+console.log(reverseString('hello'))
