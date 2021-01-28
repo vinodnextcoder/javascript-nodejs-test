bf2ae8a6b0bce6425fc2f5d497e2588611fb82fc
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2021-01-29 00:29:21 +0530

    how it's sorted, as long as it's sorted the same wa

---
+++ data_structure/string/permute.js
@@ -0,0 +1,16 @@
+var checkPermute = function(stringOne, stringTwo) {
+  // if different lengths, return false
+  if (stringOne.length !== stringTwo.length) {
+    return false;
+  // else sort and compare 
+  // (doesnt matter how it's sorted, as long as it's sorted the same way)
+  } else {
+    var sortedStringOne = stringOne.split('').sort().join('');
+    var sortedStringTwo = stringTwo.split('').sort().join('');
+    console.log(sortedStringOne,sortedStringTwo)
+    return sortedStringOne === sortedStringTwo;
+  }
+};
+
+// Tests
+console.log(checkPermute('aba', 'aab'), true);
