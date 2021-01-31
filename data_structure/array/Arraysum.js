dad35945331b9b0ebb14707e966731a39c63ae11
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2021-01-31 23:56:19 +0530

    Returns true if the there is a subarray of arr[] with sum

--- data_structure/array/Arraysum.js
+++ data_structure/array/Arraysum.js
@@ -1,28 +1,26 @@
 function subArraySum(arr, n, sum) {
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n - 1; i++) {
     curr_sum = arr[i]
     j = i + 1
-    while (j <= n) {
+    while (j <= n - 1) {
       if (curr_sum == sum) {
         console.log("indexes % d and % d" % (i, j - 1))
         return 1
       }
-      if (curr_sum > sum || j == n)
+      if (curr_sum > sum || j == n) {
         break
-
+      }
       curr_sum = curr_sum + arr[j]
       j += 1
     }
-    print("No subarray found")
-    return 0
   }
+  console.log("No subarray found")
+  return 0
+}
+let arr = [15, 2, 4, 8, 9, 5, 10, 23]
+// let n = 7
+let sum = 23
 
-arr = [15, 2, 4, 8, 9, 5, 10, 23]
-n = 8
-sum = 23
-
-subArraySum(arr, n, sum)
+console.log(subArraySum(arr, 8, sum))
     
        
-
-}
